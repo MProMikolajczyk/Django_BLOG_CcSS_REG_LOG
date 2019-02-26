62f7be5fb36384886c456774c37ec1329440e27d
mmpro <marekmikolajczyk87@gmail.com>
2019-02-26 15:47:42 +0100

    Create,Update,Delete,ListView

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
 from .views import (
-    home,
     about,
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+
 )
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', about, name='blog-about'),
     ]
